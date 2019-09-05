2258bf64ae275f3def7f71620618f085a621b943
snoopydoggie <53892985+snoopydoggie@users.noreply.github.com>
2019-09-05 12:55:07 +0530

    Delete 3b-randompass.py

--- 3b-randompass.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-def randomString():
-    pw = string.ascii_letters + string.digits + string.punctuation
-    size = random.randint(8,12)
-    return ''.join(random.choice(pw) for i in range(size))
-print("Random String with the combination of lowercase and uppercase letters")
-print ("Random String is ", randomString() )
-
